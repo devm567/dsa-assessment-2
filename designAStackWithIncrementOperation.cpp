d7f21e31ba63a6ccb0fef48f4d1dccf84fe98555
devm567 <157451001+devm567@users.noreply.github.com>
2024-08-01 23:11:26 +0530

    Create designAStackWithIncrementOperation.cpp

---
+++ designAStackWithIncrementOperation.cpp
@@ -0,0 +1,32 @@
+class CustomStack {
+public:
+    vector<int> st;
+    int n;
+    CustomStack(int maxSize) {
+        n = maxSize;
+    }
+    
+    void push(int x) {
+        if(st.size() < n){
+            st.push_back(x);
+        }
+    }
+    
+    int pop() {
+        if(st.size() != 0){
+            int x = st[st.size() - 1];
+            st.pop_back();
+            return x;
+        }
+        return -1;
+    }
+    
+    void increment(int k, int val) {
+        int a;
+        if(k <= st.size()) a = k;
+        else a = st.size();
+        for(int i = 0; i < a;i++){
+            st[i] += val;
+        }
+    }
+};
